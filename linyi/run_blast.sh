26ae4bc03740b1d4eba22a608ce43ea2d7bb0c54
Ubuntu <ubuntu@ip-172-31-30-203.us-east-2.compute.internal>
2018-04-28 17:23:35 +0000

    switches to xml output

--- linyi/run_blast.sh
+++ linyi/run_blast.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-blastn -out /pipeline/compbio2/linyi/emx1.blast.csv -outfmt 10 -query /data/compbio2/linyi/EMX1-WT.r1.consolidated.fasta -db /pipeline/compbio2/linyi/emx1.db
+blastn -out /pipeline/compbio2/linyi/emx1.blast.xml -outfmt 5 -query /data/compbio2/linyi/EMX1-WT.r1.consolidated.fasta -db /pipeline/compbio2/linyi/emx1.db
